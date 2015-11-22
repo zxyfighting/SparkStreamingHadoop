2611c7fad38d1205795fa95916448a631b9abd47
Ievgen Prokhorenko <ievgen.prokhorenko@onapp.com>
2015-11-22 21:57:49 +0200

    change scala version; add dependencies

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,3 +1,29 @@
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._ // not necessary since Spark 1.3
+
+// Create a local StreamingContext with two working thread and batch interval of 1 second.
+// The master requires 2 cores to prevent from a starvation scenario.
+
+
 object ReceiverBased {
-  def main(args: Array[String]) = println("Hello, World!")
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+    val ssc = new StreamingContext(conf, Seconds(1))
+
+    // Create a DStream that will connect to hostname:port, like localhost:9999
+    val lines = ssc.socketTextStream("localhost", 9999)
+
+    // Split each line into words
+    val words = lines.flatMap(_.split(" "))
+
+    val pairs = words.map(word => (word, 1))
+    val wordCounts = pairs.reduceByKey(_ + _)
+
+    // Print the first ten elements of each RDD generated in this DStream to the console
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
 }
