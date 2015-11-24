0fbc38a8975e27d381005e1d8b8df8e0bebd4c97
Ievgen Prokhorenko <ievgen.prokhorenko@onapp.com>
2015-11-24 22:34:29 +0200

    migrate from sbt to mvn

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,26 +1,19 @@
 import org.apache.spark._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._ // not necessary since Spark 1.3
 
-// Create a local StreamingContext with two working thread and batch interval of 1 second.
-// The master requires 2 cores to prevent from a starvation scenario.
-
+import org.apache.spark.streaming.StreamingContext._ // try to remove this
 
 object ReceiverBased {
   def main(args: Array[String]) {
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(10))
 
-    // Create a DStream that will connect to hostname:port, like localhost:9999
     val lines = ssc.socketTextStream("localhost", 9999)
 
-    // Split each line into words
     val words = lines.flatMap(_.split(" "))
-
     val pairs = words.map(word => (word, 1))
     val wordCounts = pairs.reduceByKey(_ + _)
 
-    // Print the first ten elements of each RDD generated in this DStream to the console
     wordCounts.print()
 
     ssc.start()
