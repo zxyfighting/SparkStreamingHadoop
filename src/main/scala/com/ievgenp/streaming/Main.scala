35f82a3735c6ab4df2feffd7a080d7140a2da9fb
Ievgen Prokhorenko <ievgen.prokhorenko@onapp.com>
2015-11-28 21:55:21 +0200

    add ShutdownHookThread

--- src/main/scala/com/ievgenp/streaming/Main.scala
+++ src/main/scala/com/ievgenp/streaming/Main.scala
@@ -3,6 +3,7 @@ package com.ievgenp.streaming
 import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
+import org.apache.log4j.Logger
 
 object ReceiverBased {
   def main(args: Array[String]) {
@@ -11,6 +12,9 @@ object ReceiverBased {
     val Array(zkQuorum, group, master, topics, numThreads) = args
     val sparkConf = new SparkConf().setMaster(master).setAppName("ReceiverBasedStreamingApp")
     val streamingContext = new StreamingContext(sparkConf, Seconds(5))
+    val Log = Logger.getLogger(ReceiverBased.this.getClass().getSimpleName())
+
+    Log.info("DEBUG info:" + zkQuorum)
 
     streamingContext.checkpoint("checkpoint")
 
@@ -18,6 +22,11 @@ object ReceiverBased {
     val lines = KafkaUtils.createStream(streamingContext, zkQuorum, group, topicMap).map(_._2)
 
     lines.print()
+
+    sys.ShutdownHookThread {
+      Log.info("Goodbye.")
+      streamingContext.stop(true, true)
+    }
 
     streamingContext.start()
     streamingContext.awaitTermination()
