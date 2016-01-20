349943df4715ad76758927cdfa4504d5ac219472
Ievgen Prokhorenko <ievgen.prokhorenko@onapp.com>
2016-01-20 17:36:08 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 $SPARK_HOME/bin/spark-submit \
   --class com.ievgenp.streaming.ReceiverBased \
-  target/basic-streaming-1.0-SNAPSHOT.jar zoo1 my-consumer-group local[2] test 2
+  snapshot/basic-streaming-1.0-SNAPSHOT.jar zoo1 my-consumer-group local[2] test 2
