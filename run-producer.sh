fc5454de83c40bf2269d284f0d7095243c799a3a
Eugen Prokhorenko <ievgen.prokhorenko@onapp.com>
2016-02-17 02:15:12 +0200

    update runner scripts

--- run-producer.sh
+++ run-producer.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list localhost:6667 --topic test
+/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list zulu-1.cluster:6667 --topic test
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 $SPARK_HOME/bin/spark-submit \
   --class com.eugen.streaming.ReceiverBased \
-  snapshot/basic-streaming-1.0-SNAPSHOT.jar zoo1 my-consumer-group local[2] test 2 lines-out.txt
+  snapshot/basic-streaming-1.0-SNAPSHOT.jar 0.0.0.0 my-consumer-group local[2] test 2 lines-out.txt
